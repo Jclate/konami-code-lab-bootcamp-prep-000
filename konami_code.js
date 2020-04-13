8f6e478f4fb55713934094eb749e6a00263acbd0
Jake G <jake098877@gmail.com>
2020-04-13 14:59:45 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,30 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+
+
+     var input = document.getElementsByTagName("body")
+     input.addEventListener("keydown", (event) => {
+      var index = 0
+      const key = event.key
+      var num = codes.length
+      console.log('event', key);
+
+      if (key === codes[index]) {
+
+        if (index === num - 1) {
+        alert('Congratulations!')
+
+          index = 0;
+
+        }
+        console.log('index', index);
+      index++
+      return index
+
+      } else  {
+        index = 0;
+      }
+
+  })
 }
